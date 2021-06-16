cab8032215db4fb2ed50a97806fdb1ce3cd58972
Grigory Sadovskiy <sadovskiy.grigory@yandex.ru>
2021-06-16 13:12:05 +0300

    Working erase func

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
-let matix = 50 * 50;
+// Defining the maximum amount of cells.
+
+let matix = 64 * 64;
 let gridItems = document.getElementById('grid');
+
+function getRandomColor() {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
 
 for (let i = 0; i < matix; i++) {
     let div = document.createElement('div');
@@ -18,13 +25,8 @@ let allCells = document.querySelectorAll(".cells");
     }
 })()
 
-function erase() {
-    for (let cell of allCells) {
-        cell.style.backgroundColor = "white";
-    }
-}
 
-let sliderValue = document.getElementById("slider")
+let sliderValue = document.getElementById("slider").value;
 let root = document.documentElement
 
 slider.addEventListener("input", (e) => {
@@ -32,9 +34,42 @@ slider.addEventListener("input", (e) => {
     erase();
 });
 
-let eraseButton = document.querySelector("button")
+
+
+
+
+
+
+
+// Erase button.
+// How many squares per side? (Maximum: 64)
+
+
+
+function erase() {
+    for (let cell of allCells) {
+        cell.style.backgroundColor = "white";
+    }
+}
+
+let eraseButton = document.getElementById("erase");
 eraseButton.addEventListener("click", function() {
+    let squares = prompt('How many squares per side? (Maximum: 64)');
+    root.style.setProperty("--cells-default", "repeat(" + squares + ", 1fr)")
     erase();
 })
 
+// Colorful button.
 
+let colorfulButton = document.getElementById("colorful");
+colorfulButton.addEventListener("click", function() {
+    colorize();
+})
+
+function colorize() {
+    for (let cell of allCells) {
+        cell.addEventListener("mouseenter", function() {
+           this.style.backgroundColor = "#" + getRandomColor();
+        })
+    }
+}
